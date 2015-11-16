157f38db592c1fee40d036f8d48824f2c5f09f8a
Hassanmir92 <hassanmir92@gmail.com>
2015-11-16 16:36:43 +0000

    set up project model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   name: {type: String, required: true},
+  bio: String,
   twitter: String,
   github: String,
   image: String,
+  portfolio: String,
   projects:[{ type: mongoose.Schema.ObjectId, ref: 'Project' }]
 });
 
